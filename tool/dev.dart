0b94e60f8575ff126ab4730845e9da6d3258c49e
Claire Sarsam <claire.sarsam@workiva.com>
2017-04-14 14:52:54 -0700

    Set up test runner

--- tool/dev.dart
+++ tool/dev.dart
@@ -1,6 +1,6 @@
 library tool.dev;
 
-import 'package:dart_dev/dart_dev.dart' show dev, config;
+import 'package:dart_dev/dart_dev.dart' show Environment, TestRunnerConfig, config, dev;
 
 main(List<String> args) async {
   // https://github.com/Workiva/dart_dev
@@ -10,7 +10,34 @@ main(List<String> args) async {
   config.format
     ..directories = directories
     ..lineLength = 100;
-  config.test.unitTests = [];
+
+  config.genTestRunner.configs = [
+    new TestRunnerConfig(
+        genHtml: true,
+        directory: 'test/unit',
+        filename: 'generated_runner_test',
+        dartHeaders: const [
+          "import 'package:react/react_client.dart';",
+          "import 'package:web_skin_dart/ui_core.dart';"
+        ],
+        preTestCommands: const [
+          'setClientConfiguration();',
+          'enableTestMode();'
+        ],
+        htmlHeaders: const [
+          '<script src="packages/web_skin/dist/js/core/modernizr/modernizr-custom.js"></script>',
+          '<script src="packages/react/react_with_addons.js"></script>',
+          '<script src="packages/react/react_dom.js"></script>'
+        ])
+  ];
+  config.test.unitTests = ['test/unit/generated_runner_test.dart'];
+
+  config.taskRunner.tasksToRun = [
+    'pub run dart_dev format --check',
+    'pub run dart_dev analyze',
+    'pub run dart_dev gen-test-runner --check',
+    'pub run dart_dev test'
+  ];
 
   await dev(args);
 }
