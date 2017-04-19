8948c97dc69b219de387c3a7e7bbf586dc308559
Greg Littlefield <greg.littlefield@workiva.com>
2017-04-19 10:15:57 -0700

    TodoListItem - Add completion checkbox
    set-branch ui-workshop/5b-item-notes ui-workshop/5a-item-checkbox_done

--- lib/src/components/todo_list_item.dart
+++ lib/src/components/todo_list_item.dart
@@ -1,12 +1,17 @@
-import 'package:todo_sdk/todo_sdk.dart';
-import 'package:web_skin_dart/ui_core.dart';
+import 'package:react/react.dart' as react;
+import 'package:react/react_client.dart';
+import 'package:todo_sdk/todo_sdk.dart' show Todo;
 import 'package:web_skin_dart/ui_components.dart';
+import 'package:web_skin_dart/ui_core.dart';
+
+import 'package:todo_client/src/actions.dart' show TodoActions;
 
 @Factory()
 UiFactory<TodoListItemProps> TodoListItem;
 
 @Props()
 class TodoListItemProps extends UiProps {
+  TodoActions actions;
   Todo todo;
 }
 
@@ -17,8 +22,42 @@ class TodoListItemComponent extends UiComponent<TodoListItemProps> {
 
   @override
   render() {
-    return (ListGroupItem()..className = 'todo-list__item')(
+    var classes = forwardingClassNameBuilder()
+      ..add('todo-list__item')
+      ..add('todo-list__item--complete', props.todo.isCompleted)
+      ..add('todo-list__item--incomplete', !props.todo.isCompleted);
+
+    return (ListGroupItem()..className = classes.toClassName())(
+      Block()(
+        // Row 1, Column 1: "shrink-wrapped" width (checkbox)
+        (Block()..shrink = true)(
+          _renderTaskCheckbox(),
+        ),
+        // Row 1, Column 2: (task name)
+        BlockContent()(
+          _renderTaskHeader(),
+        ),
+      ),
+    );
+  }
+
+  ReactElement _renderTaskCheckbox() {
+    return (CheckboxInput()
+      ..checked = props.todo.isCompleted
+      ..label = 'Complete Task'
+      ..hideLabel = true
+      // In theory this would be some unique id that your app could keep track of for data persistence
+      ..value = ''
+      ..onChange = _toggleCompletion)();
+  }
+
+  ReactElement _renderTaskHeader() {
+    return Dom.div()(
       props.todo.description,
     );
+  }
+
+  void _toggleCompletion(react.SyntheticFormEvent event) {
+    props.actions.updateTodo(props.todo.change(isCompleted: !props.todo.isCompleted));
   }
 }
