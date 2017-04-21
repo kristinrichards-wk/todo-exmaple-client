4ad38b8e319ddc4e254a89303d95ff05f0e9cea3
Greg Littlefield <greg.littlefield@workiva.com>
2017-04-21 12:27:24 -0700

    TodoListItem tests boilerplate
    set-branch ui-workshop/8-tests

---
+++ test/unit/components/todo_list_item_test.dart
@@ -0,0 +1,18 @@
+import 'dart:html';
+
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+import 'package:todo_sdk/todo_sdk.dart';
+import 'package:web_skin_dart/test_util.dart';
+import 'package:web_skin_dart/ui_core.dart';
+import 'package:web_skin_dart/ui_components.dart';
+
+import 'package:todo_client/src/components/todo_list_item.dart';
+
+import '../mocks.dart';
+
+main() {
+  group('TodoListItem', () {
+
+  });
+}
